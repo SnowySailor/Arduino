167d91468d724216b9ed77592e9036e1ea1fb014
SnowySailor <kanel.nick@gmail.com>
2016-02-17 13:28:15 -0500

    lab 6

--- AEV_Controller/_01_myCode.ino
+++ AEV_Controller/_01_myCode.ino
@@ -1,21 +1,8 @@
 void myCode() {
     //Accelerate
-    celerate(4,0,25,3);
-    //Go for 1 second
-    goFor(1);
-    //Set both mototrs to 20
-    setBoth(20);
-    //Go for 2 seconds
-    goFor(2);
-    //Reverse both
-    reverse(4);
-    //Set both to 25
-    setBoth(25);   
-    //Go for 2 seconds
-    goFor(2);
-    //Stop both
+    setBot(20);
+    goToRelativePosition(505);
     brake(4);
-
 } // DO NOT REMOVE. end of void myCode()
 
 void setBoth(int speed) {
