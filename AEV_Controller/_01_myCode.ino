589390937fbcfce5bb163f9cae4ecf218c8532ae
SnowySailor <kanel.nick@gmail.com>
2016-02-24 13:22:57 -0500

    Lab 7 code. Commented out stop function.

--- AEV_Controller/_01_myCode.ino
+++ AEV_Controller/_01_myCode.ino
@@ -1,11 +1,42 @@
 void myCode() {
     //Accelerate
     setBoth(20);
-    goToRelativePosition(505);
+    goToRelativePosition(322);
+    reverse(4);
+    setBoth(20);
+    goFor(1);
+    goToRelativePosition(-322);
     brake(4);
-} // DO NOT REMOVE. end of void myCode()
+    //int direction = getVehicleDirection();
+    //stop(direction);
+}
 
 void setBoth(int speed) {
     motorSpeed(1,speed);
     motorSpeed(2,speed);
 }
+
+
+// void stop(int dir) {
+//     reverse(4);
+//     setBoth(20);
+//     int positions[5] = {0,0,0,0,0};
+//     while(!hasStopped(positions)) {
+//         goFor(0.06);
+//         int newPos = getVehiclePostion();
+//         positions[4] = positions[3];
+//         positions[3] = positions[2];
+//         positions[2] = positions[1];
+//         positions[1] = positions[0];
+//         positions[0] = newPos;
+//         return pos;
+//     }
+//     brake(4);
+// }
+
+// bool hasStopped(int positions[]) {
+//     if(positions[2] > positions[5] && positions[2] < positions[0]) {
+//         return true;
+//     }
+//     return false;
+// }
