0738a698e2f15162cf91cc2363d95f3a6d189431
SnowySailor <kanel.nick@gmail.com>
2016-03-02 10:43:31 -0500

    Add comments.

--- AEV_Controller/_01_myCode.ino
+++ AEV_Controller/_01_myCode.ino
@@ -1,17 +1,27 @@
 void myCode() {
-    //Accelerate
+    //Reverse motors
     reverse(4);
+    //Set both motors to 25
     setBoth(25);
+    //Go to the end of the track
     goToRelativePosition(-290);
+    //Reverse the motors
     reverse(4);
+    //Set them both to 20 to reverse direction
     setBoth(20);
+    //Go for 1 second.
     goFor(1);
+    //Go back to the origin
     goToRelativePosition(290);
+    //Reverse the motors
     reverse(4);
     //int direction = getVehicleDirection();
     //stop(direction);
+    //Speed them up to 35 to stop the AEV
     setBoth(35);
+    //Go for 1.5 seconds to stop it
     goFor(1.5);
+    //Stop motors.
     brake(4);
 }
 
@@ -22,10 +32,14 @@ void setBoth(int speed) {
 
 
 void stop(int dir) {
+    //Reverse the direction of motors.
     reverse(4);
-    setBoth(20);
+    //Set both motors to 30
+    setBoth(30);
+    //While the AEV hasn't stopped
     while(!(getVehicleDirection() == 2)) {
         //Do literally nothing
     }
+    //Stop motors once it is stopped.
     brake(4);
 }
