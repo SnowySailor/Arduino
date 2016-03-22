3e1c4c94919daaa46339b334c7191c4387c9b9c9
SnowySailor <kanel.nick@gmail.com>
2016-03-22 13:22:55 -0400

    Should run

--- AEV_Controller/_01_myCode.ino
+++ AEV_Controller/_01_myCode.ino
@@ -3,11 +3,30 @@ void myCode() {
     reverse(4);
     //Set both motors to 25
     setBoth(25);
-    goToRelativePosition(-800);
+    //Start at station
+    goToRelativePosition(177);
+    stop(getVehicleDirection());
+    //Do hook stuff
+    goFor(2);
+    setBoth(25);
+    
+    //Go to the first stop
+    goToRelativePosition(-172);
+    stop(getVehicleDirection());
+    goFor(2);
+
+    //Go to second stop
+    setBoth(25);
+    goToRelativePosition(-270);
+    stop(getVehicleDirection());
+    goFor(2);
+
+    //Stop at the end to let them off
+    setBoth(29);
+    goToRelativePosition(-197);
     stop(getVehicleDirection());
     goFor(4);
-    setBoth(25);
-    goToRelativePosition(800);
+
     stop(getVehicleDirection());
 }
 
@@ -22,11 +41,14 @@ void stop(int dir) {
     //Reverse the direction of motors.
     reverse(4);
     //Set both motors to 30
-    celerate(4,0,30,1.5);
+    celerate(4,0,30,1);
     //While the AEV hasn't stopped
-    while(getVehicleDirection() != dir) {
+    while(getVehicleDirection() == dir) {
         //Do literally nothing
     }
     //Stop motors once it is stopped.
     brake(4);
 }
+
+
+//d = 0.0124*marks
