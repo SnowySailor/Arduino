f0595ceb14b242a8eb139ebc17cf7b3513e47fcb
Marcus Pereira <Marcus Pereira>
2016-03-24 13:42:48 -0400

    Boring message

--- AEV_Controller/_01_myCode.ino
+++ AEV_Controller/_01_myCode.ino
@@ -1,50 +1,51 @@
 void myCode() {
     //Reverse motors
     //Set both motors to 25
-    setBoth(25);
+    int speed = 23;
+    setBoth(speed);
     //Start at station
-    goToRelativePosition(157);
-    stop(getVehicleDirection());
+    goToRelativePosition(145);
+    stop(getVehicleDirection(), true);
     //Do hook stuff
     goFor(2);
-    setBoth(25);
+    setBoth(speed);
     
     //Go to the first stop
     goToRelativePosition(-152);
-    stop(getVehicleDirection());
+    stop(getVehicleDirection(), true);
     goFor(2);
 
     //Go to second stop
     reverse(4);
-    setBoth(25);
+    setBoth(speed);
     goToRelativePosition(-260);
-    stop(getVehicleDirection());
+    stop(getVehicleDirection(), true);
     goFor(2);
 
     //Stop at the end to let them off
     reverse(4);
-    setBoth(35);
-    goToRelativePosition(-247);
-    stop(getVehicleDirection());
+    setBoth(speed+15);
+    goToRelativePosition(-227);
+    stop(getVehicleDirection(), true);
     goFor(4);
 
     //Go to third stop
     setBoth(17);
-    goToRelativePosition(120);
-    stop(getVehicleDirection());
+    goToRelativePosition(110);
+    stop(getVehicleDirection(), true);
     goFor(2);
 
     //Go to GC
     reverse(4);
-    setBoth(20);
-    goToRelativePosition(422);
-    stop(getVehicleDirection());
+    setBoth(speed);
+    goToRelativePosition(442);
+    stop(getVehicleDirection(), true);
     goFor(2);
 
     //Go to maintenance station
-    setBoth(25);
+    setBoth(speed);
     goToRelativePosition(-152);
-    stop(getVehicleDirection());
+    stop(getVehicleDirection(), true);
 }
 
 void setBoth(int speed) {
@@ -53,7 +54,7 @@ void setBoth(int speed) {
 }
 
 
-void stop(int dir) {
+void stop(int dir, boolean thing) {
     setBoth(0);
     //Reverse the direction of motors.
     reverse(4);
@@ -70,6 +71,14 @@ void stop(int dir) {
     }
     //Stop motors once it is stopped.
     brake(4);
+    if(thing) {
+        reverse(4);
+        setBoth(40);
+        goFor(0.45);
+        reverse(4);
+    }
+    brake(4);
+   
 }
 
 
